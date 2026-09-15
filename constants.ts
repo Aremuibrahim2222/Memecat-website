533bcdcb6b4122428dc44e296d30ed97599775cc
Aremuibrahim2222 <167606483+Aremuibrahim2222@users.noreply.github.com>
2026-09-15 07:25:10 +0100

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,9 @@
+export const CONTRACT_ADDRESS =
+  "0x66E0CbD1b8134Ede90AC0649A054C5fafd5c5104";
+
+export const LINKS = {
+  x: "https://x.com/aiminicat",
+  telegram: "https://t.me/ArtificialMiniCat",
+  dexscreener:
+    "https://dexscreener.com/robinhood/0x30b1a8acaf798148354154bd228d1bfda37f03a732785fa5fdd0b0bac59db1ba",
+};
